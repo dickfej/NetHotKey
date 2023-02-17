7f712d16b9591a1600e04585a1f78370852f2056
Ken Fong <dickfej@gmail.com>
2023-02-17 14:41:30 +0800

    HotKeySettingBox 由UserControl改为TextBox派生

--- HotKeySettingBox.cs
+++ HotKeySettingBox.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms;
 
 namespace NetHotKey {
-  public partial class HotKeySettingBox : UserControl {
+  public partial class HotKeySettingBox : TextBox {
 
     private HotKey _hotKey = new HotKey();
 
@@ -11,19 +12,30 @@ namespace NetHotKey {
     public HotKey HotKey { get { return _hotKey; } set { 
         if(value!= null) {
           _hotKey = value;
-          txtBox.Text = _hotKey.ToString();
+          Text = _hotKey.ToString();
         }
       } 
     }
 
-    public HotKeySettingBox() {
-      InitializeComponent();
-      txtBox.BackColor = System.Drawing.SystemColors.Window;
-    }
+    //public HotKeySettingBox() {
+    //  InitializeComponent();
+    //  txtBox.BackColor = System.Drawing.SystemColors.Window;
+    //}
 
     private void txtBox_KeyDown(object sender, KeyEventArgs e) {
       HotKey = HotKeyHelper.KeyEventArgs2HotKeySetting(e);
     }
+
+    protected override void OnKeyDown(KeyEventArgs e) {
+      //base.OnKeyDown(e);
+      HotKey = HotKeyHelper.KeyEventArgs2HotKeySetting(e);
+    }
+
+    protected override void OnTextChanged(EventArgs e) {
+      //base.OnTextChanged(e);
+      Text = _hotKey.ToString() ;
+    }
+
   }
 
 }
